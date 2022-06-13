6a05028c83242eba7705579b72ce18e4a00a7ac1
Manish Jaiswal <jaiswal.manish49@gmail.com>
2022-06-13 09:29:20 +0530

    stateless implementation completed

---
+++ src/app/course-card-list/course-card-list.component.ts
@@ -0,0 +1,44 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { filter, tap } from 'rxjs/operators';
+import { CourseDialogComponent } from '../course-dialog/course-dialog.component';
+import { Course } from '../model/course';
+
+@Component({
+  selector: 'course-card-list',
+  templateUrl: './course-card-list.component.html',
+  styleUrls: ['./course-card-list.component.scss']
+})
+export class CourseCardListComponent implements OnInit {
+
+  @Input() courses: Course[] = [];
+
+  @Output() coursesChange = new EventEmitter()
+
+  constructor(private dialog: MatDialog) { }
+
+  ngOnInit(): void {
+  }
+
+  editCourse(course: Course) {
+
+    const dialogConfig = new MatDialogConfig();
+
+    dialogConfig.disableClose = true;
+    dialogConfig.autoFocus = true;
+    dialogConfig.width = "400px";
+
+    dialogConfig.data = course;
+
+    const dialogRef = this.dialog.open(CourseDialogComponent, dialogConfig);
+
+    dialogRef.afterClosed()
+             .pipe(
+               filter(value => !!value),
+               tap(() => this.coursesChange.emit())
+             )
+              .subscribe()
+
+  }
+
+}
